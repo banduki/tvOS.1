a514f8dc2ef17dcf9257932e1f7def98d6b99086
Daniel Buki <7684445+banduki@users.noreply.github.com>
2020-04-22 11:30:42 -0500

    Add files via upload

---
+++ tvOS.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tvOS.1
+//
+//  Created by Daniel Buki on 4/12/20.
+//  Copyright © 2020 Daniel Buki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!\nDaniel is the best.")
+    }
+}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+////        ContentView()
+//    }
+//}
---
+++ tvOS.1Tests/tvOS_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  tvOS_1Tests.swift
+//  tvOS.1Tests
+//
+//  Created by Daniel Buki on 4/12/20.
+//  Copyright © 2020 Daniel Buki. All rights reserved.
+//
+
+import XCTest
+@testable import tvOS_1
+
+class tvOS_1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
